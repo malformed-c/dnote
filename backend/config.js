4a6d7a6fcd41e5444a3168b79b4b113a3ae133ea
Malformed C <enderman08@gmail.com>
2024-04-28 16:29:24 +0000

    rm -rf old

--- backend/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'API',
-  env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000,
-  base_url: process.env.BASE_URL || 'http://backend:3000',
-  db: {
-    uri: process.env.REDIS_URI || 'redis://redis:6379',
-  },
-};
